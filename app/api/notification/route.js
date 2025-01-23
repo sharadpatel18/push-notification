eed9922d1de0a0660556e8635d91cb7c2204459d
sharad patel <sharadpatel1807@gmail.com>
2025-01-23 10:44:58 +0530

    updates

--- app/api/notification/route.js
+++ app/api/notification/route.js
@@ -6,9 +6,16 @@ const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
       projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: privateKey,
+      clientId: process.env.FIREBASE_CLIENT_ID,
+      authUri: process.env.FIREBASE_AUTH_URI,
+      tokenUri: process.env.FIREBASE_TOKEN_URI,
+      authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
     }),
   });
 }
